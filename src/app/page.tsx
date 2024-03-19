c83ef04acf1253b55786f30dc0e83751de5963b4
TranDangKhoi <baits16@gmail.com>
2024-03-19 22:44:50 +0700

    :+1: Update next config for unsplash domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import Card from "src/app/components/Card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Card></Card>
+      <Image
+        src="https://plus.unsplash.com/premium_photo-1681412205470-77848a519359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Whatever"
+        width={500}
+        height={500}
+      ></Image>
+      {/* <Card></Card> */}
     </main>
   );
 }
