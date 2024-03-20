a35fbd1e68e557bdbe099f5e13ecc0d1df24e8bf
TranDangKhoi <baits16@gmail.com>
2024-03-20 18:35:57 +0700

    😲 Redirecting inside a server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import ButtonRedirect from "src/app/components/ButtonRedirect";
+import { redirect } from "next/navigation";
+
+const isLoggedIn = false;
 
 export default function Home() {
+  if (!isLoggedIn) {
+    redirect("/login");
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex items-center gap-3">
@@ -14,7 +18,7 @@ export default function Home() {
         <button type="button" onClick={() => router.push("/register")}>
           Register Page
         </button> */}
-        <ButtonRedirect></ButtonRedirect>
+        {/* <ButtonRedirect></ButtonRedirect> */}
       </div>
       <Image
         src="https://plus.unsplash.com/premium_photo-1681412205470-77848a519359?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
