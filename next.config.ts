3b29056e0c7ca2e113555be84e59fe7c94927074
Trần Đăng Khôi <baits16@gmail.com>
2025-02-18 09:35:20 +0700

    👍 Remove redundant files after bumping to v15

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        pathname: "/**",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
